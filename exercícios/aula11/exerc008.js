c6b2db76d4435c1338ec085bb95b204ae33beb63
Leonardo Thome <158233688+LeoDev991@users.noreply.github.com>
2025-12-04 12:30:17 -0300

    add JavaScript

---
+++ exercícios/aula11/exerc008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log(`A sua velocidade atual é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade, você foi MULTADO!!')
+
+}
+console.log('Dirija sempre com segurança')
