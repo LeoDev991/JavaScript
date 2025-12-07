364b85b8b986c45d7b456881988c69dc7dca84ae
Leonardo Thome <158233688+LeoDev991@users.noreply.github.com>
2025-12-07 19:57:35 -0300

    Add age verifier exercise for aula14
    Introduces the 'Verificador de Idade' exercise in exercicios/aula14, including HTML, CSS, JavaScript, and related images. The app allows users to input birth year and gender, then displays age and an appropriate image based on the selection.

---
+++ exercícios/aula14/script.js
@@ -0,0 +1,60 @@
+function verificar() {
+    var data = new Date();
+    var anoAtual = data.getFullYear();
+    var fano = document.getElementById('iage');
+    var res = document.getElementById('res');
+
+    // validação
+    if (fano.value.length == 0 || Number(fano.value) > anoAtual) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!');
+        return; // não continua se estiver errado
+    }
+
+    var fsex = document.getElementsByName('isex');
+    var idade = anoAtual - Number(fano.value);
+    var genero = '';
+
+    if (fsex[0].checked) {
+        genero = 'Homem';
+    } else {
+        genero = 'Mulher';
+    }
+
+    res.style.textAlign = 'center';
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
+
+    // preparar área da foto (limpa conteúdo anterior)
+    var fotoDiv = document.getElementById('foto');
+    fotoDiv.innerHTML = '';
+
+    // cria imagem e seta atributos (uso de mesma pasta "imagens")
+    var img = document.createElement('img');
+    img.setAttribute('id', 'imagem');
+    img.style.borderRadius = '50%';
+    img.width = 250;
+    img.height = 250;
+
+    if (genero === 'Homem') {
+        if (idade >= 0 && idade < 10) {
+            img.src = 'imagens/homem-bebe.jpg';
+        } else if (idade < 21) {
+            img.src = 'imagens/homem-jovem.jpg';
+        } else if (idade < 50) {
+            img.src = 'imagens/homem-adulto.jpg';
+        } else {
+            img.src = 'imagens/homem-idoso.jpg';
+        }
+    } else {
+        if (idade >= 0 && idade < 10) {
+            img.src = 'imagens/mulher-bebe.jpg';
+        } else if (idade < 21) {
+            img.src = 'imagens/mulher-jovem.jpg';
+        } else if (idade < 50) {
+            img.src = 'imagens/mulher-adulta.jpg';
+        } else {
+            img.src = 'imagens/mulher-idosa.jpg';
+        }
+    }
+
+    fotoDiv.appendChild(img);
+}
